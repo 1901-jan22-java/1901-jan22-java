41aad11d30d635fd5d93abc7a3e813c86e7bf881
maw <malikiw20@gmail.com>
2019-01-23 10:33:18 -0500

    int to hex example

---
+++ Week1/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld{
+// this is a single line 
+
+/** Multi Line 
+Comments**/
+
+// Main Method 
+public static void main (String [] args){
+    
+    System.out.println("Hello World! "); 
+    System.out.println(" You passed in " + args.length + " parameters(s)");
+}
+
+
+}
+
+
+
+class HelloWorld2{
+    
+     
+}
