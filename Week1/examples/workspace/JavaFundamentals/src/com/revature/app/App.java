c858c16170649c3390d809726f9300297a1c125b
genesisb17 <genesis.bonds@gmail.com>
2019-01-23 10:09:28 -0500

    wednesday morning work

---
+++ Week1/examples/workspace/JavaFundamentals/src/com/revature/app/App.java
@@ -0,0 +1,13 @@
+package com.revature.app;
+
+import com.revature.classbasics.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		Blocks.test();
+		Blocks b = new Blocks();
+		Blocks c = new Blocks();
+	}
+
+}
