03241cbbf61619002352c2623c63bcda2871c4cc
Andrew Simmons <Andrew.simmons@live.com>
2019-01-23 10:52:54 -0500

    pushing up work from day 1 and 2

---
+++ Andrew_Simmons_Code/assignment one.java
@@ -0,0 +1,21 @@
+
+//access modifer class class name
+
+public class Hello World
+{
+
+
+//this is a single line comment 
+
+System.out.println("Hello World");
+			
+
+
+
+
+/*
+Anything within these symbols will not be interpreted by the compiler
+*/
+
+
+}
