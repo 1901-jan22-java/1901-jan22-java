a58fade727675d7fb47595ad73006e9ec7716f39
unknown <hhe@mailfence.com>
2019-01-24 14:32:05 -0500

    change row in Q13 to list

--- Henry_He_Code/Assignment#1/src/question_13/Triangle.java
+++ Henry_He_Code/Assignment#1/src/question_13/Triangle.java
@@ -1,11 +1,12 @@
 package question_13;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Triangle {
 
 	static void printTriangle(int n) {
-		ArrayList<Integer> row = new ArrayList<>();
+		List<Integer> row = new ArrayList<>();
 		for(int i=0; i<n; i++) {
 			row.add( 0 , i%2 );
 			for(int num : row) {
