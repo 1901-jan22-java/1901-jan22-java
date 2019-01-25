efa9db5d100d80a6099507e539a7b69fb8ac3d8b
Michael Koerber <Mkoerber7@gmail.com>
2019-01-24 21:34:26 -0500

    Fixed HelloWorld doc name

--- Week1/examples/HelloWorld.java
+++ Week1/examples/HelloWorld.java
@@ -1,24 +1,5 @@
-//package com.revature.hello
-
-//import java.util.List;
-
-//[access mod] class [class name]{}
-public class HelloWorld{
-// this is a single line comment 
-
-/*
-anything within these symbols will 
-not be interpreted by the compiler
-(multi-line comment)
-*/
-	
-	//MAIN METHOD! - starting point of java programs
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("You passed in "
-		+ args.length + " parameter(s)");
+public class HelloWorlds{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
-}
-
-class HelloWorld2{
 }
