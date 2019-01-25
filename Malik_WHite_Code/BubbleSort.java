ce7b12f4b1f7d7df41e2e2df36657d26e0c1505b
maw <malikiw20@gmail.com>
2019-01-25 12:20:30 -0500

    Bubble Sort Tweak

--- Malik_WHite_Code/BubbleSort.java
+++ Malik_WHite_Code/BubbleSort.java
@@ -5,65 +5,65 @@ package com.revature.codechallenges;
 	
 	int count = 0; 
 	
+	static void printList( int [] x) {
+		
+		for(int i = 0; i < x.length; i++) {
+			System.out.print(x[i] + " ");
+		}
+	}
+	
 	
 	static void bSort (int [ ] a ) {
 		
-		//temp array 
-		boolean swapped; 
-		int [ ] temp = new int [a.length]; 
+		
+
+		int temp; 
 		 
 		
 		
-		for (int j = 0; j < a.length - 1; j ++)	{
-		for (int i = 0 ; i < a.length - j  - 1; i++ ) {
-			
-			swapped = false; 
-			if ( a[i] > a[i+1]) {
-				
-					temp[i] = a[i+1];
-					a[i+1] = a[i] ; 
-					a[i] = temp[i]; 
-					swapped = true;
-					
-			}
-			
-			if (swapped == false) {
-				
-				break; 
-				
-			}
-		}}
-		
-//		if (count == 0) {
-//			System.out.println("The Array is Sorted");
-//		}
-//		
-//		else {
-//			
-//		}
+		 for (int i = 0; i < a.length-1; i++) {
+	            for (int j = 0; j < a.length-i-1; j++) {
+	                if (a[j] > a[j+1]) 
+	                { 
+	                    temp = a[j]; 
+	                    a[j] = a[j+1]; 
+	                    a[j+1] = temp; 
+	                    
+	                    
+	                } 
+	    } } BubbleSort.printList(a);
+
+}
 		
 		
 		
 		
-	}
+	
 	
 
 	public static void main (String [] args) {
-		int a [] = new int [5]; 
-		a[0] = 5; 
-		a[1] = 4;
-		a[2] = 3;
-		a[3] = 2;
-		a[4] = 1;
+
 		
-		for (int i = 0; i < a.length; i ++) {
-		System.out.print(a[i] + " ");}
+	int [] a = new int [10]; 
 		
+		a[0] = 1; 
+		a[1] = 0;
+		a[2] = 5;
+		a[3] = 6;
+		a[4] = 3;
+		a[5] = 2;
+		a[6] = 3;
+		a[7] = 7;
+		a[9] = 9;
+		a[8] = 8;
+		a[4] = 4;
+		
+		
+	
+		BubbleSort.printList(a); 
+		System.out.println();
 		BubbleSort.bSort( a); 
-		
-		System.out.println(); 
-		for (int i = 0; i < a.length; i ++) {
-			System.out.print(a[i] + " ");}
+
 		
 		
 	}
