f84573e50318e3f43ebf0456c313c5c0d136b47f
unknown <hhe@mailfence.com>
2019-01-27 18:36:02 -0500

    Fixed question#6 and included lotsa comments

--- Henry_He_Code/Assignment#1/src/question_06/Fancy_Even_Check.java
+++ Henry_He_Code/Assignment#1/src/question_06/Fancy_Even_Check.java
@@ -2,12 +2,26 @@ package question_06;
 
 public class Fancy_Even_Check {
 
-	/* It's fortunate that I've encountered this problem before...
-	 * Hint: 32 = 10000
-	 *       31 = 01111
-	 */
+	
+	// This isEven method actually checks for powers of two...
+//	static boolean isEven(int n) {
+//		return (n & n-1) == 0;
+//	}
+	
 	static boolean isEven(int n) {
-		return (n & n-1) == 0;
+		// Had an interesting discussion with Malik where he checked if
+		// n/2 * 2 == n <- exploits the property of even numbers
+//		return ( (n/2)*2 == n);
+		
+		//I had a different idea (see below)
+		String binary = Integer.toBinaryString(n);
+		System.out.println(binary.charAt(binary.length()-1));
+		return binary.charAt(binary.length()-1) == '0';
+		
+		// Should be able to just do this:
+		// return Integer.lowestOneBit(n) == 0;
+		// For some reason, it's not working as intended.
+		
 	}
 	
 	static public void main(String args[]) {
