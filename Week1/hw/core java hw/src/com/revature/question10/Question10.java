7d84a6654ca2098078a34ea13cdbafbbf29a70c2
Kevin Ho <kevho482@gmail.com>
2019-01-27 15:21:58 -0500

    added question 10

--- Week1/hw/core java hw/src/com/revature/question10/Question10.java
+++ Week1/hw/core java hw/src/com/revature/question10/Question10.java
@@ -1,10 +1,33 @@
 package com.revature.question10;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Question10 {
 
+	/*
+	 * Find the minimum of two numbers using ternary operators
+	 * */
 	public static void main(String[] args)
 	{
+		System.out.println("Enter two numbers");
+		try{
+			Scanner input = new Scanner(System.in);
+			int num1 = input.nextInt();
+			int num2 = input.nextInt();
+			input.close();
+
+			System.out.println("" + smallerNum(num1, num2) + " is the minimum");
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("Invalid input");
+		}
 		
+	}
+	
+	public static int smallerNum(int num1, int num2)
+	{
+		return num1<num2 ? num1 : num2;
 	}
 }
