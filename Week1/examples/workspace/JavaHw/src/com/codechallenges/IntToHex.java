f9f22722c81fdb1351083f39ad2b986058a94e8c
Michael Koerber <Mkoerber7@gmail.com>
2019-01-26 19:42:45 -0500

    Organized Folders

---
+++ Week1/examples/workspace/JavaHw/src/com/codechallenges/IntToHex.java
@@ -0,0 +1,25 @@
+package com.codechallenges;
+
+public class IntToHex {
+
+	public static void convert(int num){
+		StringBuilder str = new StringBuilder("");
+		char[] hex = {'a','b','c','d','e','f'};
+		while(num > 0){
+			int remainder = num % 16;
+			System.out.println(remainder);
+			if(remainder>9){
+				str.append(hex[remainder-10]);
+			} else str.append(remainder);
+			num /= 16;
+			System.out.println(str);
+		}
+		System.out.println(str.reverse());
+	}
+	
+	public static void main(String[] args) {
+		convert(1023);
+	}
+}
+
+
---
+++ michael_koerber_code/Week1Hw/src/com/question1/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.question1;
+
+public class BubbleSort {
+	
+	  static void bSort(int array[]){
+		    int n = array.length;
+		    int swapper = 0;   // Holding place for the greater value to be swapped
+		    for (int i = 0; i < n; i++){ // Loop starts at the 0 index
+		      for (int j = 1; j < n; j++){ // inner loop starts at the 1st index
+		        if (array[j-1] > array[j]){ 
+		          swapper = array[j-1]; // If the element in the index before j is greater, then add it to the swapper
+		          array[j-1] = array[j]; // The index that was before j now becomes j while the swapper holds the original value
+		          array[j] = swapper; // Original value of J is now replaced with the greater number
+		        }
+		      } // For loop runs until the if statement in the inner loop is satisfied, then the outer loop can complete.
+		    }
+		  }
+	
+	public static void printArray(int nums[]){
+		for(int i = 0; i < nums.length; i++){
+			System.out.print(nums[i] + ", ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1, 0, 5, 6, 3, 2, 3, 7, 9, 8, 4};
+		bSort(nums);
+		printArray(nums);
+	}
+}
+
+
