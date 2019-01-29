30624b61a18bd7ffb554f27e8b7f498ab439275a
Andrew Simmons <Andrew.simmons@live.com>
2019-01-29 09:15:41 -0500

    test

---
+++ Andrew_Simmons_Code/Challenges/OneChallenge.java
@@ -0,0 +1,31 @@
+package challengeOne;
+
+public class OneChallenge {
+
+	public static void main(String[] args) {
+		int points[]; // declaring an array with the INT type
+		points = new int[] {1,3,7,2,5,10,11}; // set the array size to 100
+		int sumTarget = 13;
+	    getPairsCount(points, sumTarget);
+		
+		
+	}
+	
+    // Prints number of pairs in points[0..n-1] with sum equal 
+    // to 'sumTarget' 
+    public static void getPairsCount(int[] points, int sumTarget) 
+    { 
+  
+        int count = 0;// Initialize result 
+  
+        // Consider all possible pairs and check their sums 
+        for (int i = 0; i < points.length; i++) 
+            for (int j = i + 1; j < points.length; j++) 
+                if ((points[i] + points[j]) == sumTarget) 
+                    count++; 
+  
+        System.out.printf("Count of pairs is %d",count); 
+        //The program should count that there is two pair of numbers that can equal to that sum target 13 in this case
+    } 
+
+}
