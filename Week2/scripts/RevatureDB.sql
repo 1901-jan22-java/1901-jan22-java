5f55a97644d76f961dd0f83c0fa71ca38901c538
genesisb17 <genesis.bonds@gmail.com>
2019-01-31 12:03:18 -0500

    more things on sql

--- Week2/scripts/RevatureDB.sql
+++ Week2/scripts/RevatureDB.sql
@@ -35,4 +35,73 @@ CREATE TABLE Associates(
   FOREIGN KEY(batchid) REFERENCES Batches(bid)
 );
 
+select * from roles;
+
+-- add values to our lookup table 
+insert into Roles(RID, Title) values(1, 'Associate');
+insert into Roles values(4, 'Sales Rep');
+
+
+------------------- TRIGGERS , SEQUENCES 
+/*  a sequence in a DB is a set of integers that are generated and 
+supported by the DB system itself to support the production of 
+unique values on demand 
+- A user defined schema bound object that generates a sequence 
+of numeric values 
+- we will use these more often than not to auto generate a PK value
+*/
+CREATE SEQUENCE EMP_SEQ;
+
+-- creating a sequence with the no specified settings will create one as follows
+CREATE SEQUENCE  "DEMO1901JAN22RDS"."EMP_SEQ"  
+MINVALUE 1 
+MAXVALUE 9999999999999999999999999999 
+INCREMENT BY 1 
+START WITH 1 
+CACHE 20 
+NOORDER  
+NOCYCLE 
+NOPARTITION ;
+
+drop sequence trainer_seq;
+CREATE SEQUENCE A_SEQ;
+CREATE SEQUENCE BATCH_SEQ;
+
+/* TRIGGERS
+A trigger is a special type of stored procedure that happens 
+automatically when an event occurs in the database 
+*/
+CREATE OR REPLACE TRIGGER emp_trig -- declare and name trigger
+BEFORE INSERT ON Employees -- specify when it will execute
+FOR EACH ROW -- necessary to change values in rows 
+BEGIN -- start what we want to happen
+  select emp_seq.nextval into :new.empid from dual;      
+END; -- end of what we want to happen 
+/
+
+------ DUAL
+/*
+Dual is a special one-row, one-column table present by 
+default in Oracle (and some other) database installations
+It exists for various reasons including being able to select 
+values not associated with a table
+*/
+select * from dual;
+select emp_seq.nextval from dual;
+select SYSDATE from dual;
+
+
+-------------- DML
+select * from employees;
+
+insert into employees( firstname, lastname, email, roleid)
+values('Malik', 'White', 'mw@gmail.com', 1);
+
+update employees 
+set firstname = 'John'
+where empid = 9;
+
+
+----- REMEMBER TO COMMIT!
+commit;
 
