3d1b62fc7497ca791540c46e41be183e4fc0902e
Joshua Vanderzee <joshuavanderzee@gmail.com>
2019-02-04 16:33:12 -0500

    more changes to project0 db

--- Joshua_Vanderzee_code/project0/The-Americans-Bank/src/main/java/com/revature/bank/app/App.java
+++ Joshua_Vanderzee_code/project0/The-Americans-Bank/src/main/java/com/revature/bank/app/App.java
@@ -1,11 +1,18 @@
 package com.revature.bank.app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.revature.bank.jdbc.dao.UserAccountControler;
 import com.revature.bank.jdbc.pojos.User;
 
+/**
+ * @author thejo
+ *
+ */
 public class App {
 
 	public static void main(String[] args) {
@@ -15,38 +22,75 @@ public class App {
 			switch (choice("Create an account", "Login to account")) {
 			case 1:
 				while (active) {
-					String UserId, UserName, Password, FirstName, LastName, SecurityQuestion1, SecurityAnswer1, SecurityQuestion2, SecurityAnswer2, SecurityQuestion3, SecurityAnswer3, Addressline1, Addressline2, City, States, Country, PostalCode, HomePhone, CellPhone, Fax, Email, SSN, BirthDate, Maritalstatus;
-					UserId = java.sql.
-					UserName, 
-					Password, 
-					FirstName, 
-					LastName, 
-					SecurityQuestion1, 
-					SecurityAnswer1, 
-					SecurityQuestion2, 
-					SecurityAnswer2, 
-					SecurityQuestion3, 
-					SecurityAnswer3, 
-					Addressline1, 
-					Addressline2, 
-					City, 
-					States, 
-					Country, 
-					PostalCode, 
-					HomePhone,
-					CellPhone, 
-					Fax, 
-					Email, 
-					SSN, 
-					BirthDate, 
-					Maritalstatus
+					String UserName, Password, FirstName, LastName, 
+					SecurityQuestion1, SecurityAnswer1, SecurityQuestion2, SecurityAnswer2, SecurityQuestion3, SecurityAnswer3, 
+					Addressline1, Addressline2, City, States, Country, Email, Maritalstatus;
+					int UserId, SSN, PostalCode, HomePhone, CellPhone, Fax; 
+					Date BirthDate;
+					
+					UserId = -1;
+					System.out.print("Username: ");
+					UserName = getUserInput();
+					System.out.print("Username: ");
+					Password = getUserInput();
+					System.out.print("Username: ");
+					FirstName = getUserInput();
+					System.out.print("Username: ");
+					LastName = getUserInput();
+					System.out.print("Username: ");
+					SecurityQuestion1 = getUserInput();
+					System.out.print("Username: ");
+					SecurityAnswer1 = getUserInput();
+					System.out.print("Username: ");
+					SecurityQuestion2 = getUserInput();
+					System.out.print("Username: ");
+					SecurityAnswer2 = getUserInput();
+					System.out.print("Username: ");
+					SecurityQuestion3 = getUserInput();
+					System.out.print("Username: ");
+					SecurityAnswer3 = getUserInput();
+					System.out.print("Username: ");
+					Addressline1 = getUserInput();
+					System.out.print("Username: ");
+					Addressline2 = getUserInput();
+					System.out.print("Username: ");
+					City = getUserInput();
+					System.out.print("Username: ");
+					States = getUserInput();
+					System.out.print("Username: ");
+					Country = getUserInput();
+					System.out.print("Username: ");
+					PostalCode = getUserInputInt();
+					System.out.print("Username: ");
+					HomePhone = getUserInputInt();
+					System.out.print("Username: ");
+					CellPhone = getUserInputInt();
+					System.out.print("Username: ");
+					Fax = getUserInputInt();
+					System.out.print("Username: ");
+					Email = getUserInput();
+					System.out.print("Username: ");
+					SSN = Integer.parseInt(getUserInput());
+					SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+					while (true) {
+						System.out.print("Username: ");
+						try {
+							BirthDate = sdf.parse(getUserInput());
+							break;
+						} catch (ParseException e) {
+							System.out.println("Invalid Date. ");
+						}
+					}
+					System.out.print("Username: ");
+					Maritalstatus = getUserInput();
+					
+					User user = new User(UserId, UserName, Password, FirstName, LastName, 
+							SecurityQuestion1, SecurityAnswer1, SecurityQuestion2, SecurityAnswer2, SecurityQuestion3, SecurityAnswer3, 
+							Addressline1, Addressline2, City, States, Country, PostalCode, HomePhone, CellPhone, Fax, 
+							Email, SSN, BirthDate, Maritalstatus);
 					
 					
-					
-					User user = new User(UserId, UserName, Password, FirstName, LastName, SecurityQuestion1, SecurityAnswer1, SecurityQuestion2, SecurityAnswer2, SecurityQuestion3, SecurityAnswer3, Addressline1, Addressline2, City, States, Country, PostalCode, HomePhone, CellPhone, Fax, Email, SSN, BirthDate, Maritalstatus)
-					
-					
-					UserAccountControler.CreateAccount(user)
+					UserAccountControler.CreateAccount(user);
 				}
 				break;
 			case 2:				
@@ -70,6 +114,15 @@ public class App {
 		}
 	}
 	
+	private static int getUserInputInt() {
+		while (true) {
+			try (Scanner c = new Scanner(System.in)){
+				return Integer.parseInt(c.nextLine());
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input");
+			}
+		}
+	}
 	
 	private static int choice(String... str) {
 		while (true) {
