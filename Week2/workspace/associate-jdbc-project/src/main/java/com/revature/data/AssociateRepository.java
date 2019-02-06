8e906d031f598acc6fdba626b989e66408036795
genesisb17 <genesis.bonds@gmail.com>
2019-02-06 15:57:02 -0500

    starting java part of associate assignment

---
+++ Week2/workspace/associate-jdbc-project/src/main/java/com/revature/data/AssociateRepository.java
@@ -0,0 +1,29 @@
+package com.revature.data;
+
+import java.util.List;
+
+import com.revature.pojos.Associate;
+
+public class AssociateRepository {
+	
+	public List<Associate> getAllAssociates(){
+		return null;
+	}
+	
+	public int getBestAssociate() {
+		return 0;
+	}
+	
+	public Associate getById(int id) {
+		return null;
+	}
+	
+	public Associate update(Associate a) {
+		return null;
+	}
+	
+	public Associate save(Associate a) {
+		return null;
+	}
+
+}
