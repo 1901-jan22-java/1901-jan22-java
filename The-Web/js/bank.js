db2c3c1947b72568abb09c58e15c5b53ccbe5f29
Kevin Ho <kevho482@gmail.com>
2019-02-08 21:08:56 -0500

    added ajax bank

--- The-Web/js/bank.js
+++ The-Web/js/bank.js
@@ -1,6 +1,55 @@
 window.onload = function(){
     $('#register').on('click', addUser);
     $('#uname').on('blur', isUnique);
+    $('#login').on('click', logIn);
+}
+
+function logIn(){
+    var username = $('#unamelog').val();
+    var password = $('#passlog');
+    
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            var user = JSON.parse(xhr.responseText);
+            if(user.length == 0){
+                alert("Invalid credentials!");
+            } else{
+                var logged = user[0];
+                if(logged.password == password){
+                    alert("welcome, " + logged.firstName);
+                }
+                else{
+                    alert("Invalid credentials!");
+                }
+            }
+        }
+    }
+    xhr.open("GET", `http://localhost:3000/users?username=${username}`);
+    xhr.send();
+}
+function displayUserAccounts(user){
+    var xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            console.log(xhr.status + ": " + xhr.statusText);
+            console.log(xhr.getAllResponseHeaders);
+            var accounts = JSON.parse(xhr.responseText);
+
+            if(accounts.length ==0){
+                $('body').html(`Hey ${user.firstName}, you don't have any accounts! Let's change that!`);
+            }
+            else{
+                var text = `You have ${accounts.length} accounts(s): `;
+                for(let a of accounts){
+                    text += `<br>Account No.${a.id} has a balance of ${a.balance}`;
+                }
+                $('body').html(text);
+            }
+        }
+    }
+    xhr.open("GET", `http://localhost:3000/accounts?userId=${user.id}`);
+    xhr.send();
 }
 
 function addUser(){
