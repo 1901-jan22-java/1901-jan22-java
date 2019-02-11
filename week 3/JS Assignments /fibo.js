c5156c9f903dc657062c18e5f077b5ed21ddff91
maw <malikiw20@gmail.com>
2019-02-11 12:00:47 -0500

    JS assignments

---
+++ week 3/JS Assignments /fibo.js
@@ -0,0 +1,19 @@
+//1. Fibonacci Define function: fib(n) Return the nth number in the fibonacci sequence.
+
+function fib (n){
+
+    let temp = 1; 
+    let add; 
+    let arr = []; 
+
+    for( i = 0; i <= n; i++){
+
+        add = i + temp; 
+        temp = add; 
+        a[i] = add; 
+
+    }
+
+console.log(a[n]); 
+
+}
