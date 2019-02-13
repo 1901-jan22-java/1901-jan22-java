4a8d27787c86bc314529856e104d777f90184d25
genesisb17 <genesis.bonds@gmail.com>
2019-02-13 13:51:12 -0500

    intro to forwards

---
+++ The-Web/servlets/hello-servlets/src/main/webapp/scripts/userApp.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    $('#goToSubmit').on('click', loadRegisterView);
+    $('#logIn').on('click', logIn);
+}
+
+function logIn(){
+    //function to log user in 
+}
+
+function loadRegisterView(){
+    //function to load view to add new user
+    console.log("load register view function");
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            $('#view').html(xhr.responseText);
+        }
+    }
+    xhr.open("GET", "loadRegView");
+    xhr.send();
+}
