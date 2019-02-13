6e8774cba7223a2842e7eceef2a53abee83b8527
Sanford Zheng <sanfordzheng@gmail.com>
2019-02-13 17:22:17 -0500

    Edited and formatted code

--- Sanford_Zheng_Code/servlet-practice/src/main/java/com/revature/servlets/LoginServlet.java
+++ Sanford_Zheng_Code/servlet-practice/src/main/java/com/revature/servlets/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.services.UserService;
@@ -17,6 +20,7 @@ import com.revature.services.pojos.User;
 public class LoginServlet extends HttpServlet {
 
 	UserService service = new UserService();
+	private static Logger log = Logger.getLogger(LoginServlet.class);
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -26,15 +30,21 @@ public class LoginServlet extends HttpServlet {
 
 		User storedUser = service.getByUsername(u.getUsername());
 
-		if (storedUser == null || !storedUser.getPassword().equals(u.getPassword())) {
+		if (storedUser == null) {
 			resp.setStatus(418);
+		} else if (!storedUser.getPassword().equals(u.getPassword())) {
+			resp.setStatus(409);
 		} else {
 			resp.setStatus(200);
 			
 			HttpSession session = req.getSession();
 			session.setAttribute("user", u);
 			
+			log.info("CREATED SESSION " + session.getId());
 			
+			PrintWriter writer = resp.getWriter();
+			resp.setContentType("application/json");
+			writer.write(mapper.writeValueAsString(userStored));
 		}
 	}
 
