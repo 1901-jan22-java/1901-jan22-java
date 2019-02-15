3e85b951899e15774e6a1dc94b03cb4ea7c99ec6
unknown <hhe@mailfence.com>
2019-02-15 13:27:12 -0500

    setup work

---
+++ Henry_He_Code/project_1/src/main/java/com/jdbc/util/ConnectionFactory.java
@@ -0,0 +1,60 @@
+package com.jdbc.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class ConnectionFactory {
+	
+	final static Logger logger = Logger.getLogger(ConnectionFactory.class);
+	
+	private static ConnectionFactory cf = null;
+	
+	private ConnectionFactory() {
+		logger.info("INSTANTIATED CONNECTION FACTORY");
+	}
+	
+	public static synchronized ConnectionFactory getInstance() {
+		if(cf == null) {
+			cf = new ConnectionFactory();
+		}
+		logger.info("RETURNING CF INSTANCE " + cf.getClass());
+		return cf;
+	}
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		Properties prop = new Properties();
+		String filepath = "src/main/resources/db.properties";
+		try {
+			prop.load(new FileReader(filepath));
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(
+						prop.getProperty("url"), 
+						prop.getProperty("username"),
+						prop.getProperty("pwd")
+					);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public static void main(String[] args) {
+		Connection conn = ConnectionFactory.getInstance().getConnection();
+	}
+	
+}
