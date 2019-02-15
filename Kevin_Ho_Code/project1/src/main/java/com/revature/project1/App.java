bc9240cbc6ab3c8b15b96a635daf98b9f607275b
Kevin Ho <kevho482@gmail.com>
2019-02-15 10:07:04 -0500

    created project 1 classes

--- Kevin_Ho_Code/project1/src/main/java/com/revature/project1/App.java
+++ Kevin_Ho_Code/project1/src/main/java/com/revature/project1/App.java
@@ -1,15 +1,10 @@
 package com.revature.project1;
 
-import java.util.Scanner;
-
 public class App 
 {
-	static Scanner input = new Scanner(System.in);
 	
 	public static void main( String[] args )
     {
-    	System.out.println("Hello welcome to the ERS system.\n"
-    						+"Please log in.\n");
     	
     }
 }
