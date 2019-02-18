091992d011d4cf0b8ada225c33cd41216a1f7b7d
Kevin Ho <kevho482@gmail.com>
2019-02-17 22:51:57 -0500

    project 1. added reimbursement jdbc

---
+++ Kevin_Ho_Code/project1/src/main/java/com/revature/reimbursement/Reimbursement.java
@@ -0,0 +1,104 @@
+package com.revature.reimbursement;
+
+import java.sql.Timestamp;
+
+public class Reimbursement {
+	private int reimbId;
+	private double amount;
+	private Timestamp submitted;
+	private Timestamp resolved;
+	private String description;
+	private int author;
+	private int resolver;
+	private int statusId;
+	private int typeId;
+	
+	public Reimbursement(int reimbId, double amount, Timestamp submitted, Timestamp resolved, String description, int author,
+			int resolver, int statusId, int typeId) {
+		this.reimbId = reimbId;
+		this.amount = amount;
+		this.submitted = submitted;
+		this.resolved = resolved;
+		this.description = description;
+		this.author = author;
+		this.resolver = resolver;
+		this.statusId = statusId;
+		this.typeId = typeId;
+	}
+
+	public Reimbursement() {
+	}
+
+
+	public int getReimbId() {
+		return reimbId;
+	}
+
+	public void setReimbId(int reimbId) {
+		this.reimbId = reimbId;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Timestamp getSubmitted() {
+		return submitted;
+	}
+
+	public void setSubmitted(Timestamp submitted) {
+		this.submitted = submitted;
+	}
+
+	public Timestamp getResolved() {
+		return resolved;
+	}
+
+	public void setResolved(Timestamp resolved) {
+		this.resolved = resolved;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(int author) {
+		this.author = author;
+	}
+
+	public int getResolver() {
+		return resolver;
+	}
+
+	public void setResolver(int resolver) {
+		this.resolver = resolver;
+	}
+
+	public int getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+
+	public int getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+}
---
+++ Kevin_Ho_Code/project1/src/main/java/com/revature/reimbursement/ReimbursementRepo.java
@@ -0,0 +1,172 @@
+package com.revature.reimbursement;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.user.User;
+import com.revature.util.ConnectionFactory;
+
+public class ReimbursementRepo {
+	public List<Reimbursement> getReimbursements(User user)
+	{
+		List<Reimbursement> tmp = new ArrayList<Reimbursement>();
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			String sql = "SELECT * FROM ers_reimbursement WHERE reimb_author = ?";
+
+			String[] keys = {"reimb_id"};
+			PreparedStatement ps = conn.prepareStatement(sql,keys);
+			
+			ResultSet pk = ps.executeQuery();
+
+			while(pk.next()) {
+				Reimbursement temp = new Reimbursement(	pk.getInt("reimb_id"),
+														pk.getDouble("reimb_amount"),
+														pk.getTimestamp("reimb_submitted"),
+														pk.getTimestamp("reimb_resolved"),
+														pk.getString("reimb_description"),
+														pk.getInt("reimb_author"),
+														pk.getInt("reimb_resolver"),
+														pk.getInt("reimb_status_id"),
+														pk.getInt("reimb_type_id"));
+				tmp.add(temp);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return tmp;
+	}
+	
+	public List<Reimbursement> getAllReimbursements()
+	{
+		List<Reimbursement> tmp = new ArrayList<Reimbursement>();
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			String sql = "SELECT * FROM ers_reimbursement ORDER BY reimb_submitted DESC";
+
+			Statement statement = conn.createStatement();
+			
+			ResultSet rs = statement.executeQuery(sql);
+
+			while(rs.next()) {
+				Reimbursement temp = new Reimbursement(	rs.getInt("reimb_id"),
+														rs.getDouble("reimb_amount"),
+														rs.getTimestamp("reimb_submitted"),
+														rs.getTimestamp("reimb_resolved"),
+														rs.getString("reimb_description"),
+														rs.getInt("reimb_author"),
+														rs.getInt("reimb_resolver"),
+														rs.getInt("reimb_status_id"),
+														rs.getInt("reimb_type_id"));
+				tmp.add(temp);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return tmp;
+	}
+	
+	public List<Reimbursement> getAllUnresolvedReimbursements()
+	{
+		List<Reimbursement> tmp = new ArrayList<Reimbursement>();
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			String sql = "SELECT * FROM ers_reimbursement WHERE reimb_resolved IS NULL ORDER BY reimb_submitted DESC";
+
+			String[] keys = {"reimb_id"};
+			PreparedStatement ps = conn.prepareStatement(sql,keys);
+			
+			ResultSet pk = ps.executeQuery();
+
+			while(pk.next()) {
+				Reimbursement temp = new Reimbursement(	pk.getInt("reimb_id"),
+														pk.getDouble("reimb_amount"),
+														pk.getTimestamp("reimb_submitted"),
+														pk.getTimestamp("reimb_resolved"),
+														pk.getString("reimb_description"),
+														pk.getInt("reimb_author"),
+														pk.getInt("reimb_resolver"),
+														pk.getInt("reimb_status_id"),
+														pk.getInt("reimb_type_id"));
+				tmp.add(temp);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return tmp;
+	}
+	
+	public Reimbursement addReimb(int type, int user, double amount, String desc, int status, Timestamp submitted) 
+	{
+		Reimbursement reimb = new Reimbursement();
+
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()) 
+		{
+			conn.setAutoCommit(false);
+			String sql = "INSERT INTO ers_reimbursement(reimb_type_id, reimb_author, reimb_amount, "
+													+ 	"reimb_description, reimb_status_id, reimb_submitted) "
+													+ "	VALUES(?,?,?,?,?,?)";
+
+			String[] keys = {"reimb_id"};
+			PreparedStatement ps = conn.prepareStatement(sql,keys);
+			
+			ps.setInt(1, type);
+			ps.setInt(2, user);
+			ps.setDouble(3, amount);
+			ps.setString(4, desc);
+			ps.setInt(5, status);
+			ps.setTimestamp(6, submitted);
+						
+			
+			int numRows = ps.executeUpdate();
+			
+			if(numRows > 0) {
+				ResultSet pk = ps.getGeneratedKeys();
+				pk.next();
+				reimb.setReimbId((pk.getInt(1)));
+				reimb.setTypeId(type);
+				reimb.setAmount(amount);
+				reimb.setAuthor(user);
+				reimb.setDescription(desc);
+				reimb.setStatusId(status);
+				reimb.setSubmitted(submitted);
+				conn.commit();
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return reimb;
+	}
+	
+	public Reimbursement resolve(Reimbursement reimb, User u, Timestamp status, int statusId)
+	{
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+			conn.setAutoCommit(false);
+			String sql = "{call resolve(?,?,?,?)}";
+			
+			CallableStatement cs = conn.prepareCall(sql);
+			
+			cs.setInt(1, reimb.getReimbId());
+			cs.setInt(2, u.getUserId());
+			cs.setTimestamp(3, status);
+			cs.setInt(4, statusId);
+			
+			cs.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return reimb;
+	}	
+}
