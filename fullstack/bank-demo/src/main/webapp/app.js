fb808c1bca321ced62fbbee4f97470125a0e88e4
genesisb17 <genesis.bonds@gmail.com>
2019-02-20 15:17:24 -0500

    load login view and front controller

--- fullstack/bank-demo/src/main/webapp/app.js
+++ fullstack/bank-demo/src/main/webapp/app.js
@@ -1,3 +1,19 @@
 window.onload = function(){
+	console.log("Loaded app");
+	loadLoginView();
+	
+}
+
+function loadLoginView(){
+	//retrieve login.html partial and replace #view with it
+	//then add event listeners for functionality 
+	
+	//send request using AJAX
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		//only place to deal with response
+	}
+	xhr.open("GET", "login.view");
+	xhr.send();
 	
 }
