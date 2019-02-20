96f8c8f287c1e3e251640602f84efdcaad429d5a
genesisb17 <genesis.bonds@gmail.com>
2019-02-20 16:15:09 -0500

    login functionality complete

---
+++ fullstack/bank-demo/src/main/java/com/revature/servlets/HomeServlet.java
@@ -0,0 +1,38 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import oracle.jdbc.proxy.annotation.Post;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//check if session exists, if one doesnt, do not create one
+		HttpSession session = req.getSession(false);
+		
+		if(session == null) {
+			resp.setStatus(500);
+			resp.sendRedirect("login.view");
+		}else {
+			resp.sendRedirect("home.view");
+		}
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		//get user data 
+	}
+
+}
