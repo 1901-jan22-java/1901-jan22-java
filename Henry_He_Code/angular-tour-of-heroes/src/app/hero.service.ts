866ca9e42ac15eb982b26f9c65e49af3c22aa90c
unknown <hhe@mailfence.com>
2019-02-22 13:32:46 -0500

    Completed part 5 of tutorial

--- Henry_He_Code/angular-tour-of-heroes/src/app/hero.service.ts
+++ Henry_He_Code/angular-tour-of-heroes/src/app/hero.service.ts
@@ -19,6 +19,11 @@ export class HeroService {
   //   return HEROES;
   // }
 
+getHero(id: number): Observable<Hero> {
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(HEROES.find(hero => hero.id === id));
+}
+
   constructor(private messageService: MessageService) {}
 
 
