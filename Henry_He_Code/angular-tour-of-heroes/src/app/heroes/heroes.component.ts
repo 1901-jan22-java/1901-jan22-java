3597d70a94bdeae3b32df24730701dfffefc8b6c
unknown <hhe@mailfence.com>
2019-02-22 12:52:41 -0500

    Completed part 4 of tutorial

--- Henry_He_Code/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Henry_He_Code/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   // Selector ~ Component's CSS selector
@@ -14,15 +14,29 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  // New version of getHeroes() method
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(
+        heroes => this.heroes = heroes
+      );
+  }
+
+  // Old version of getHeroes() method
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
