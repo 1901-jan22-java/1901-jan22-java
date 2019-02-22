0ecbe9fcdf08a32d00fd5e3322a803491b50164d
Kevin Ho <kevho482@gmail.com>
2019-02-22 17:10:42 -0500

    reset project to previous state. registering doesnt work

--- Kevin_Ho_Code/project1/src/main/webapp/scripts/userapp.js
+++ Kevin_Ho_Code/project1/src/main/webapp/scripts/userapp.js
@@ -54,7 +54,7 @@ function logIn(){
             }
         }
     }
-    xhr.open("POST", "login");
+    xhr.open("POST", "logIn");
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(json);
 }
