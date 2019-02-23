da90edaaae0688ce6991d0de3128612365896b44
Sanford <sanfordzheng@gmail.com>
2019-02-23 11:44:49 -0500

    renamed home.java to HomeServlet.java

--- Sanford_Zheng_Code/Z-Projects/project-1/src/main/java/com/revature/ers/servlets/HomeServlet.java
+++ Sanford_Zheng_Code/Z-Projects/project-1/src/main/java/com/revature/ers/servlets/HomeServlet.java
@@ -9,14 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.ers.dao.dto.User;
 
 /**
  * Servlet implementation class Home. I don't know what I'm doing...
  */
 @WebServlet("/home")
-public class Home extends HttpServlet {
+public class HomeServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 8550366702754639405L;
 //	private static final ObjectMapper om = new ObjectMapper();
