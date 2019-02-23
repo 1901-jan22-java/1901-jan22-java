363212c85bb8ed43e2c544a92089cd98f7f5bfd5
Sanford <sanfordzheng@gmail.com>
2019-02-22 23:27:34 -0500

    todo: session check and role check

---
+++ Sanford_Zheng_Code/Z-Projects/project-1/src/main/java/com/revature/ers/servlets/ReimbursementServlet.java
@@ -0,0 +1,49 @@
+package com.revature.ers.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.ers.services.ReimbursementService;
+
+@WebServlet("/reimbursement")
+public class ReimbursementServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6417817436028192843L;
+	private static ObjectMapper om = new ObjectMapper();
+//	private static ReimbursementService rs = new ReimbursementService();
+
+	/**
+	 * Gets all reimbursements
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String json = om.writeValueAsString(ReimbursementService.getAllReimbursements());
+		
+		resp.setContentType("application/json");
+		resp.getWriter().write(json);
+	}
+	
+	
+	/**
+	 * Add a new reimbursement request
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		String json = om.writeValueAsString(ReimbursementService.getAllReimbursements());
+		
+		resp.setContentType("application/json");
+		resp.getWriter().write(json);
+	}
+	
+}
