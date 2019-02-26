0720e65f90dfeb6dc1d916b9fc3adef0336a5815
genesisb17 <genesis.bonds@gmail.com>
2019-02-26 10:44:19 -0500

    beginning hibernate

--- fullstack/bank-demo/src/main/java/com/revature/servlets/HomeServlet.java
+++ fullstack/bank-demo/src/main/java/com/revature/servlets/HomeServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import oracle.jdbc.proxy.annotation.Post;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
