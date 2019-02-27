81d4f0a33bd08edc344faaa444c6c45c2c860b43
genesisb17 <genesis.bonds@gmail.com>
2019-02-27 11:58:08 -0500

    adding more on multiplicity

--- Hibernate/examples/hello-hibernate/src/main/java/com/revature/app/BlogApp.java
+++ Hibernate/examples/hello-hibernate/src/main/java/com/revature/app/BlogApp.java
@@ -10,11 +10,11 @@ public class BlogApp {
 	public static void main(String[] args) {
 		PostRepository pDao = new PostRepository();
 		UserRepository uDao = new UserRepository();
-		
+	
 		User u = uDao.get(2);
-		Post p = new Post("UnderStanding Hibernate",
-				"This is so cool!", u);
+//		Post p = new Post("UnderStanding Hibernate",
+//				"This is so cool!", u);
 		
-		pDao.save(p);
+	System.out.println(pDao.getByAuthor(u));
 	}
 }
--- Hibernate/examples/hello-hibernate/src/main/java/com/revature/dao/PostRepository.java
+++ Hibernate/examples/hello-hibernate/src/main/java/com/revature/dao/PostRepository.java
@@ -1,9 +1,13 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.revature.models.Post;
+import com.revature.models.User;
 import com.revature.util.ConnectionUtil;
 
 public class PostRepository {
@@ -23,5 +27,29 @@ public class PostRepository {
 			session.close();
 		}
 		return p;
+	}
+	
+	public List<Post> getByAuthor(User u){
+		Session session = util.getSession();
+		try {
+			Query query = session.createQuery
+					("from Post where Author_id = :user");
+			query.setParameter("user", u.getId());
+			return query.list();
+		}
+		finally {
+			session.close();
+		}
+	}
+	//Calling named query
+	public List<Post> getEmptyPosts(){
+		Session session = util.getSession();
+		try {
+			Query query = session.getNamedQuery("getEmptyPosts");
+			return query.list();
+		}
+		finally {
+			session.close();
+		}
 	}
 }
