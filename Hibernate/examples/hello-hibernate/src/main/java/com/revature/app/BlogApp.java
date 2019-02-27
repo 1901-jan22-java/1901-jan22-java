c4c1ee721852b8b8e7e015f1674a4e79b5a6b384
genesisb17 <genesis.bonds@gmail.com>
2019-02-27 11:30:00 -0500

    multiplicity

---
+++ Hibernate/examples/hello-hibernate/src/main/java/com/revature/app/BlogApp.java
@@ -0,0 +1,20 @@
+package com.revature.app;
+
+import com.revature.dao.PostRepository;
+import com.revature.dao.UserRepository;
+import com.revature.models.Post;
+import com.revature.models.User;
+
+public class BlogApp {
+
+	public static void main(String[] args) {
+		PostRepository pDao = new PostRepository();
+		UserRepository uDao = new UserRepository();
+		
+		User u = uDao.get(2);
+		Post p = new Post("UnderStanding Hibernate",
+				"This is so cool!", u);
+		
+		pDao.save(p);
+	}
+}
--- Hibernate/examples/hello-hibernate/src/main/java/com/revature/dao/PostRepository.java
+++ Hibernate/examples/hello-hibernate/src/main/java/com/revature/dao/PostRepository.java
@@ -1,9 +1,27 @@
 package com.revature.dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.Post;
 import com.revature.util.ConnectionUtil;
 
 public class PostRepository {
 	
 	static ConnectionUtil util = ConnectionUtil.getInstance();
 
+	
+	public Post save(Post p) {
+		Session session = util.getSession();
+		try {
+			Transaction tx = session.beginTransaction();
+			int id = (int) session.save(p);
+			tx.commit();
+			p.setId(id);
+		}
+		finally {
+			session.close();
+		}
+		return p;
+	}
 }
