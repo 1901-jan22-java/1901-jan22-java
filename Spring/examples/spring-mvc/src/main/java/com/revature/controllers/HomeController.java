7ecccc326b34b9d8b5883ff4cb6ce26901fe5a0b
genesisb17 <genesis.bonds@gmail.com>
2019-03-07 12:00:06 -0500

    exploring requests with spring mvc

--- Spring/examples/spring-mvc/src/main/java/com/revature/controllers/HomeController.java
+++ Spring/examples/spring-mvc/src/main/java/com/revature/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.revature.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,24 @@ public class HomeController {
 	@ResponseBody //necessary annotation when using @Controller to indicate that what is returned from this method is the response body and not the name of a resource to be resolve
 	public String home() {
 		return "Welcome to Spring MVC!";
+	}
+	
+	/*
+	 * With controllers, we can have multiple of the same HTTP
+	 * verb methods in the same class, HOWEVER, they cannot be 
+	 * addressed to the same exact URL or else there will be 
+	 * conflicts. 
+	 * Using @RequestMapping with a new value allows us to 
+	 * send requests to different methods in a class based 
+	 * on a url pattern
+	 * ie: /user = GET ALL
+	 * 		/user/{id} =  GET BY ID
+	 */
+	@RequestMapping(method=RequestMethod.GET,
+			value="/{name}")
+	@ResponseBody
+	public String anotherGetRequest(@PathVariable String name) {
+		return "Welcome home, " + name;
 	}
 
 }
