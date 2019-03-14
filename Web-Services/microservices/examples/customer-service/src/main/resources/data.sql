47d144f5468e3d5d81266e6d537e8733e6d2aafc
genesisb17 <genesis.bonds@gmail.com>
2019-03-14 15:08:06 -0400

    web services continued

---
+++ Web-Services/microservices/examples/customer-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customer(USERNAME, PASSWORD) values('genesis', 'password');
+INSERT INTO Customer(USERNAME, PASSWORD) values('testuser', 'admin');
+INSERT INTO Customer(USERNAME, PASSWORD) values('abc', '123');
+INSERT INTO Customer(USERNAME, PASSWORD) values('revature', 'word');
+INSERT INTO Customer(USERNAME, PASSWORD) values('ravi', 'pass');
+
