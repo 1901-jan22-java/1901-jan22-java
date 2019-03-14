24deb52f5e822071a652031ad1c74203bc0a68c1
genesisb17 <genesis.bonds@gmail.com>
2019-03-14 15:17:49 -0400

    customer service

--- Web-Services/microservices/examples/customer-service/src/main/resources/data.sql
+++ Web-Services/microservices/examples/customer-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO Customer(USERNAME, PASSWORD) values('genesis', 'password');
-INSERT INTO Customer(USERNAME, PASSWORD) values('testuser', 'admin');
-INSERT INTO Customer(USERNAME, PASSWORD) values('abc', '123');
-INSERT INTO Customer(USERNAME, PASSWORD) values('revature', 'word');
-INSERT INTO Customer(USERNAME, PASSWORD) values('ravi', 'pass');
+INSERT INTO Customer(ID, USERNAME, PASSWORD) values(1, 'genesis', 'password');
+INSERT INTO Customer(ID, USERNAME, PASSWORD) values(2, 'testuser', 'admin');
+INSERT INTO Customer(ID, USERNAME, PASSWORD) values(3, 'abc', '123');
+INSERT INTO Customer(ID, USERNAME, PASSWORD) values(4, 'revature', 'word');
+INSERT INTO Customer(ID, USERNAME, PASSWORD) values(5, 'ravi', 'pass');
 
